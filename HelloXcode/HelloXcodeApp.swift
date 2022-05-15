e7a5f0c57edf903eaf269133990e3983d5740d60
davidgohu7 <davidgohu7@outlook.com>
2022-05-15 10:05:40 -0500

    Initial Commit

---
+++ HelloXcode/HelloXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloXcodeApp.swift
+//  HelloXcode
+//
+//  Created by David on 15/05/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
